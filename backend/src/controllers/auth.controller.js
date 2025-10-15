652ea1468a58bcfce70920fec28cec1a37236bcf
Muhammad5Ali <mohammad.bscs4490@iiu.edu.pk>
2025-10-15 13:25:36 +0500

    authcontroller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,9 +34,9 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      // before CR:
-      generateToken(newUser._id, res);
-      await newUser.save();
+       // Persist user first, then issue auth cookie
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
